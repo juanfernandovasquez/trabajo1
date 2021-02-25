68371692ae1ef7a92b95b46fc8dba3b8c65481e2
Juan Vásquez <juanvasquezvergara@gmail.com>
2021-02-24 23:58:31 -0500

    listo el la página, por lo que veo. Le puse la sección de contacto

--- js/slider.js
+++ js/slider.js
@@ -17,7 +17,7 @@ slides()
 
 function slides(){
     slider.style.transform = "translate(" + (-width*contador) + "px)"
-    slider.style.transition = "transform 2s"
+    slider.style.transition = "transform 1s"
     contador++
 
     if(contador == sliderIndividual.length){
